433cf194f2ccfa3191160878dd100f63e4d2106f
Naman <naman13064@iiitd.ac.in>
2015-10-26 18:35:05 +0530

    Created OfferingViewActivity

--- app/src/main/java/com/mc/priveil/gourmetpadosmein/OfferingViewActivity.java
+++ app/src/main/java/com/mc/priveil/gourmetpadosmein/OfferingViewActivity.java
@@ -3,7 +3,6 @@ package com.mc.priveil.gourmetpadosmein;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -13,16 +12,14 @@ import android.view.MenuItem;
 
 import com.mc.priveil.gourmetpadosmein.Fragments.OfferingFragment;
 import com.parse.FindCallback;
-import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class OfferingListActivity extends AppCompatActivity {
+public class OfferingViewActivity extends AppCompatActivity {
     public final static String MESSAGE_NAME = "com.mc.priveil.gourmetpadosmein.NAME";
     public final static String MESSAGE_EMAIL = "com.mc.priveil.gourmetpadosmein.EMAIL";
 
@@ -35,26 +32,23 @@ public class OfferingListActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
     private Toolbar mToolbar;
     private ActionBarDrawerToggle mActionBarDrawerToggle;
+    private String objectid;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_offerings);
-        Intent intent = getIntent();
-        name = intent.getStringExtra(MESSAGE_NAME);
-        email = intent.getStringExtra(MESSAGE_EMAIL);
-
-        Log.i("Testing12", "Came here in Listing class");
+        setContentView(R.layout.activity_offering_view);
         setUpToolbar();
         setUpNavDrawer();
 
-        Parse.initialize(this, YOUR_APPLICATION_ID, YOUR_CLIENT_KEY);
         ParseUser.enableAutomaticUser();
 
-        Log.i("Testing12", "Came here in Listing class 2");
+        Intent intent = getIntent();
+        objectid = intent.getStringExtra("objectid");
 
         ParseQuery query = new ParseQuery("Offering");
-//        query.whereEqualTo("name", "aaa");
+        query.whereEqualTo("objectId", objectid);
         query.findInBackground(new FindCallback() {
             @Override
             public void done(List list, ParseException e) {
@@ -74,45 +68,15 @@ public class OfferingListActivity extends AppCompatActivity {
                 if (o != null) {
                     List<ParseObject> itemlist;
                     itemlist = (List<ParseObject>) o;
-                    Log.i("LIST", itemlist.get(0).get("name").toString());
+                    Log.i("LIST", objectid);
+                    Log.i("LIST", itemlist.toString());
 
-                    Bundle bundle = new Bundle();
-                    ArrayList<String> names = new ArrayList<String>();
-                    for (ParseObject p : itemlist) {
-                        names.add(p.get("name").toString());
-                    }
-
-                    Log.d("List", String.valueOf(names));
-
-                    ArrayList<ArrayList<String>> cuisines = new ArrayList<ArrayList<String>>();
-                    for (ParseObject p : itemlist) {
-                        cuisines.add((ArrayList<String>) p.get("cuisine"));
-                    }
-                    Log.d("List", String.valueOf(cuisines));
-
-                    ArrayList<String> object_ids = new ArrayList<String>();
-                    for (ParseObject p : itemlist) {
-                        object_ids.add(p.getObjectId());
-                    }
-
-
-                    bundle.putSerializable("names", names);
-                    bundle.putSerializable("cuisines", cuisines);
-                    bundle.putSerializable("object_ids", object_ids);
-
-                    OfferingFragment fragment = new OfferingFragment();
-                    fragment.setArguments(bundle);
-
-                    FragmentManager fragmentManager = getSupportFragmentManager();
-                    fragmentManager.beginTransaction()
-                            .replace(R.id.content_frame, fragment).commit();
                 } else
                     Log.i("Error!!", "NULL");
             }
         });
 
-
-		/* Use application class to maintain global state. */
+        	/* Use application class to maintain global state. */
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         if (navigationView != null) {
             setupDrawerContent(navigationView);
@@ -166,12 +130,12 @@ public class OfferingListActivity extends AppCompatActivity {
                                         .replace(R.id.content_frame, new OfferingFragment()).commit();
                                 break;
                             case R.id.profile:
-                                Intent ui = new Intent(OfferingListActivity.this, UserInfo.class);
-                                ui.putExtra(MESSAGE_NAME, name);
-                                ui.putExtra(MESSAGE_EMAIL, email);
-
+//                                Intent ui = new Intent(OfferingListActivity.this, UserInfo.class);
+//                                ui.putExtra(MESSAGE_NAME, name);
+//                                ui.putExtra(MESSAGE_EMAIL, email);
+//
 //                                startActivity(intent);
-                                startActivity(ui);
+//                                startActivity(ui);
                                 break;
                         }
 
@@ -182,4 +146,5 @@ public class OfferingListActivity extends AppCompatActivity {
         );
         Log.i("Testing12", "Came out setUpDrawer");
     }
+
 }
