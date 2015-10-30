0bef25d6bbf37c47be8baaff78d6503be6f3022c
Talha Ahmad Siddiqui <talhaahmad12111@iiitd.ac.in>
2015-10-31 05:29:52 +0530

    Refresh option added

--- app/src/main/java/com/mc/priveil/gourmetpadosmein/OfferingListActivity.java
+++ app/src/main/java/com/mc/priveil/gourmetpadosmein/OfferingListActivity.java
@@ -9,10 +9,12 @@ import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.mc.priveil.gourmetpadosmein.Fragments.OfferingFragment;
@@ -66,12 +68,39 @@ public class OfferingListActivity extends AppCompatActivity implements LocationL
 
     }
     static String fromSkip = "N";
+    SwipeRefreshLayout mSwipeRefreshLayout;
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            // Check if user triggered a refresh:
+            case R.id.menu_refresh:
+                Log.i("talha123", "Refresh menu item selected");
+
+                // Signal SwipeRefreshLayout to start the progress indicator
+//                mSwipeRefreshLayout.setRefreshing(true);
+
+                Log.i("talha123", "Refresh2 menu item selected");
+
+                // Start the refresh background task.
+                // This method calls setRefreshing(false) when it's finished.
+                myUpdateOperation();
+
+                return true;
+        }
+
+        // User didn't trigger a refresh, let the superclass handle this action
+        return super.onOptionsItemSelected(item);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offerings);
         Intent intent = getIntent();
+
         name = intent.getStringExtra(MESSAGE_NAME);
         email = intent.getStringExtra(MESSAGE_EMAIL);
 
@@ -281,6 +310,129 @@ public class OfferingListActivity extends AppCompatActivity implements LocationL
 
     }
 
+    public void myUpdateOperation()
+    {
+        Log.i("refreshTest","It came here!!");
+        ParseQuery query = new ParseQuery("Offering");
+//        query.whereEqualTo("name", "aaa");
+        query.findInBackground(new FindCallback() {
+            @Override
+            public void done(List list, ParseException e) {
+                if (e == null) {
+                    if (!list.isEmpty()) {
+                        Log.i("Testing", list.get(0).toString());
+                    } else {
+                        Log.i("Testing", "List returned by Parse is empty!");
+                    }
+                } else {
+                    Log.i("Error!!", "Error in querying parse!");
+                }
+            }
+
+            @Override
+            public void done(Object o, Throwable throwable) {
+                if (o != null) {
+                    List<ParseObject> itemlist;
+                    itemlist = (List<ParseObject>) o;
+                    Log.i("LIST", itemlist.get(0).get("name").toString());
+
+                    Bundle bundle = new Bundle();
+                    ArrayList<String> names = new ArrayList<String>();
+                    for (ParseObject p : itemlist) {
+                        names.add(p.get("name").toString());
+                    }
+
+                    Log.d("List", String.valueOf(names));
+
+                    ArrayList<ArrayList<String>> cuisines = new ArrayList<ArrayList<String>>();
+                    for (ParseObject p : itemlist) {
+                        cuisines.add((ArrayList<String>) p.get("cuisine"));
+                    }
+                    Log.d("List", String.valueOf(cuisines));
+
+                    ArrayList<String> object_ids = new ArrayList<String>();
+                    for (ParseObject p : itemlist) {
+                        object_ids.add(p.getObjectId());
+                    }
+
+
+
+                    ArrayList<Double> distances = new ArrayList<>();
+                    for (ParseObject p : itemlist) {
+                        Double lat = Double.parseDouble((String)p.get("Latitude"));
+                        Double longi = Double.parseDouble((String) p.get("Longitude"));
+                        float[] dist = new float[1];
+                        Location.distanceBetween(currLatitude, currLongitude, lat, longi, dist);
+                        Double distance = (double)dist[0];
+                        distances.add(distance);
+                    }
+                    Log.d("List", String.valueOf(cuisines));
+
+                    ArrayList<String> costs = new ArrayList<>();
+                    for (ParseObject p : itemlist) {
+                        costs.add(p.get("cost").toString());
+                    }
+
+
+
+//                    ArrayList<ArrayList<String>> cuisines = new ArrayList<ArrayList<String>>();
+//                    for (ParseObject p : itemlist) {
+//                        cuisines.add((ArrayList<String>) p.get("cuisine"));
+//                    }
+//                    Log.d("List", String.valueOf(cuisines));
+//
+//
+//
+                    int sizeOfList = distances.size();
+                    for(int iter1 = 0 ; iter1 < sizeOfList ; iter1++)
+                    {
+                        for(int iter2 = iter1+1 ; iter2 < sizeOfList ; iter2++)
+                        {
+                            if(distances.get(iter2) < distances.get(iter1))
+                            {
+                                double temp = distances.get(iter1);
+                                distances.set(iter1,distances.get(iter2));
+                                distances.set(iter2,temp);
+
+                                String tempName = names.get(iter1);
+                                names.set(iter1,names.get(iter2));
+                                names.set(iter2,tempName);
+
+                                ArrayList<String> tempCuisine = cuisines.get(iter1);
+                                cuisines.set(iter1,cuisines.get(iter2));
+                                cuisines.set(iter2, tempCuisine);
+
+                                String tempObjectId = object_ids.get(iter1);
+                                object_ids.set(iter1, object_ids.get(iter2));
+                                object_ids.set(iter2,tempObjectId);
+
+                                String tempCost = costs.get(iter1);
+                                costs.set(iter1,costs.get(iter2));
+                                costs.set(iter2,tempCost);
+                            }
+                        }
+                    }
+
+                    bundle.putSerializable("names", names);
+                    bundle.putSerializable("cuisines", cuisines);
+                    bundle.putSerializable("distances", distances);
+                    bundle.putSerializable("object_ids", object_ids);
+                    bundle.putSerializable("costs", costs);
+
+
+                    bundle.putSerializable("email", email);
+
+                    OfferingFragment fragment = new OfferingFragment();
+                    fragment.setArguments(bundle);
+
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    fragmentManager.beginTransaction()
+                            .replace(R.id.content_frame, fragment).commit();
+                } else
+                    Log.i("Error!!", "NULL");
+            }
+        });
+    }
 
     private void setUpToolbar() {
         Log.i("Testing12", "Came in setUpToolBar");
@@ -342,5 +494,11 @@ public class OfferingListActivity extends AppCompatActivity implements LocationL
                 }
         );
         Log.i("Testing12", "Came out setUpDrawer");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 }
