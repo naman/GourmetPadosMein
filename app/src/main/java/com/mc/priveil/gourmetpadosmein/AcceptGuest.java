0e324fdbd104124ba655252076a0edc25a452aee
Srishti Sengupta <srishti.sengupta@gmail.com>
2015-11-14 23:28:36 +0530

    fixed apply button

---
+++ app/src/main/java/com/mc/priveil/gourmetpadosmein/AcceptGuest.java
@@ -0,0 +1,13 @@
+package com.mc.priveil.gourmetpadosmein;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AcceptGuest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accept_guest);
+    }
+}
