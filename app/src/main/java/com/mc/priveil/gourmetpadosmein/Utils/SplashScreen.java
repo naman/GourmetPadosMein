dd474ccdb8f4bd1cdd82dfaf505ace4e0b780ef5
Naman <naman13064@iiitd.ac.in>
2015-11-30 03:01:44 +0530

    tutorial works now

--- app/src/main/java/com/mc/priveil/gourmetpadosmein/Utils/SplashScreen.java
+++ app/src/main/java/com/mc/priveil/gourmetpadosmein/Utils/SplashScreen.java
@@ -4,62 +4,46 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import com.mc.priveil.gourmetpadosmein.R;
 
 public class SplashScreen extends AppCompatActivity {
-    String data;
-    SharedPreferences prefs = null;
-    @Override
 
+    SharedPreferences prefs = null;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        prefs = getSharedPreferences("com.mc.priveil.gourmetpadosmein", MODE_PRIVATE);
         setContentView(R.layout.activity_splash);
-//        Intent intent = getIntent();
-//        Bundle extras = intent.getExtras();
-//
-//        if(extras != null) {
-//            data = extras.getString("Bool");
-//            Log.i("firstTime",data);
-//        }
-        //thread for splash screen running
+        prefs = getSharedPreferences("com.mycompany.myAppName", MODE_PRIVATE);
 
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
+        startActivity(new Intent(SplashScreen.this, LogIn.class));
+/*        //thread for splash screen running
         Thread logoTimer = new Thread() {
             public void run() {
                 try {
                     sleep(2000);
                 } catch (InterruptedException e) {
                     Log.d("Exception", "Exception" + e);
-                } finally {
-
-                    if (prefs.getBoolean("firstrun", true)) {
-
-                        Log.i("FirstTime", "This will be printed just once!");
-
-                        startActivity(new Intent(SplashScreen.this, WelcomeScreen.class));
-                        prefs.edit().putBoolean("firstrun", false).commit();
-
-                    } else {
-
-                        startActivity(new Intent(SplashScreen.this, LogIn.class));
-                    }
+                }finally{
 
 
                 }
                 finish();
             }
         };
-        logoTimer.start();
-
-
+        logoTimer.start();*/
     }
 
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (prefs.getBoolean("firstrun", true)) {
+            // Do first run stuff here then set 'firstrun' as false
+            // using the following line to edit/commit prefs
+            prefs.edit().putBoolean("firstrun", false).commit();
+            startActivity(new Intent(SplashScreen.this, WelcomeScreen.class));
+        }
+    }
 }
